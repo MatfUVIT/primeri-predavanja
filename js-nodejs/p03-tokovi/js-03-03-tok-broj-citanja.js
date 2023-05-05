263c12fc951ed17d2c19b854b395bf352933b054
Vladimir Filipovic <vladofilipovic@hotmail.com>
2023-05-05 20:53:04 +0200

    popravio

--- js-nodejs/p03-tokovi/js-03-03-tok-broj-citanja.js
+++ js-nodejs/p03-tokovi/js-03-03-tok-broj-citanja.js
@@ -8,7 +8,7 @@ let brojac = 0;
 tokZaCitanje.on('data',
     (procitano) => {
         brojac++;
-        console.log(brojac + '---'+ procitano.length);
+        console.log(brojac);
     });
 tokZaCitanje.on('end',
     () => console.log("---\n" + brojac));
